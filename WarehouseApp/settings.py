674d1add0ef8e23b77c2614f52ca86fb8ac53d8d
Agven Muharis Rifqi <77918132+agvenmuharisrifqi@users.noreply.github.com>
2022-05-23 03:51:56 +0700

    Update settings.py

--- WarehouseApp/settings.py
+++ WarehouseApp/settings.py
@@ -155,8 +155,8 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aplikasisaya44@gmail.com'
-EMAIL_HOST_PASSWORD = 'vggsuqpgovragypq'
+EMAIL_HOST_USER = 'my email'
+EMAIL_HOST_PASSWORD = 'my password'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
